07be2a6b84837d2714f01d10947bc3efcc1a7643
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-04-10 16:39:55 +0600

    navigation-added

---
+++ src/Component/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            <h1>AppliedJobs</h1>
+        </div>
+    );
+};
+
+export default AppliedJobs;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './Component/ErrorPage';
+import Home from './Component/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App></App>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+       <RouterProvider router={router} />
   </React.StrictMode>,
 )
